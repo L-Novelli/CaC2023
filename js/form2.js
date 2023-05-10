15446e7f6a82dfa9243765efa27003c7efab3da4
Lautaro <l.novelli@outlook.com>
2023-05-10 08:40:45 -0300

    form updated

--- js/form2.js
+++ js/form2.js
@@ -1,15 +1,14 @@
 const form = document.querySelector('#signup-form');
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent form from submitting
+  event.preventDefault(); 
   
-  // Get form fields
   const name = form.elements.name.value;
   const email = form.elements.email.value;
   const comment = form.elements.comment.value;
   
-  // TODO: Do something with the form data
+
   console.log(name, email, comment);
   
-  // Reset form fields
+
   form.reset();
 });
